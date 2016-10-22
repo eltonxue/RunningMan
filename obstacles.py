37ab6ee4d0aab76760ab0c2af7738b9d12d982e5
Elton Xue <elxue@uci.edu>
2016-10-22 06:03:10 -0700

    Update
    - fixed obstacles

--- obstacles.py
+++ obstacles.py
@@ -13,22 +13,28 @@ from random import randint
 class Obstacles(Widget):
     def __init__(self, source):
         super(Obstacles, self).__init__()
-        self.image = Sprite(source = source, x = self.width * 20, y = 50)
+        self.image = Sprite(source = source, x = self.width * 30, y = 50)
         self.add_widget(self.image)
         self.size = self.image.size
-        self.image_dupe = Sprite(source = source, x = self.width * 30, y = 50)
+        self.image_dupe = Sprite(source = source, x = self.width * 44, y = 50)
         self.add_widget(self.image_dupe)
-        self.image_dupe2 = Sprite(source = source, x = self.width * 40, y = 50)
+        self.image_dupe2 = Sprite(source = source, x = self.width * 52, y = 50)
         self.add_widget(self.image_dupe2)
+        self.image_dupe3 = Sprite(source = source, x = self.width * 76, y = 50)
+        self.add_widget(self.image_dupe3)
         
     def update(self):
         self.image.x -= 4
         self.image_dupe.x -= 4
         self.image_dupe2.x -= 4
+        self.image_dupe3.x -= 4
         
-        if self.image.right <= 0:
-            self.image.x = self.width * randint(20, 25)
-        if self.image_dupe.right <= 0:
-            self.image_dupe.x = self.width * randint(35, 45)
-        if self.image_dupe2.right <= 0:
-            self.image_dupe2.x = self.width * randint(55, 65)
+        if self.image.right <= 0 - self.width:
+            self.image.x = self.width * 44
+        if self.image_dupe.right <= 0 - self.width:
+            self.image_dupe.x = self.width * 52
+        if self.image_dupe2.right <= 0 - self.width:
+            self.image_dupe2.x = self.width * 64
+        if self.image_dupe3.right <= 0 - self.width:
+            self.image_dupe3.x = self.width * 80
+        
