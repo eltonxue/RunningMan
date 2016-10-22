e6477f5d4340387aea76bf9bb077b1e32f2006cd
Elton Xue <elxue@uci.edu>
2016-10-22 03:16:42 -0700

    Update: main.py
    main.py:
    - resized platform

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Game(Widget):
     def __init__(self):
         super(Game, self).__init__()
         self.platform = Platform(source = "assets/platform/platform1.png")
-        self.size = [1600 * .20, 900 * .20]
+        self.size = [1600 * .60, 900 * .60]
         
         self.add_widget(self.platform)
         Clock.schedule_interval(self.update, 1.0/60.0)
