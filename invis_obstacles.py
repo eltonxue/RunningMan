00402176057f3adba151d23de24c693660c5c465
Chanun Sumphanphaisal <csumphan@uci.edu>
2016-10-23 03:37:43 -0700

    Added a restart system after game over

--- invis_obstacles.py
+++ invis_obstacles.py
@@ -16,7 +16,7 @@ class InvisObstacles(Widget):
         self.image_dupe3 = Sprite(source = source, x = 2000, y = 50)
         self.add_widget(self.image_dupe3)
         
-        self.change = 4
+        self.change = 3
 
         
     def update(self):
